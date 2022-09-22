f57d59b878625e832dd471ce8d5d86257f557ea5
Tuyen Tran <72810528+Tuyentrantrusting@users.noreply.github.com>
2022-09-23 02:20:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,8 +9,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
-
+    username = request.args.get('username')
+    password = request.args.get('password')
+    return "Hello {}! Your username is {}".format(name).format(username)
+    
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
