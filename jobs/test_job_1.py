5358610cd34df307f330a3b29f0dfec0de8e575f
tuyentran <tuyen.tran@trustingsocial.com>
2022-09-23 13:31:58 +0700

    raise error

--- jobs/test_job_1.py
+++ jobs/test_job_1.py
@@ -1,19 +1,10 @@
-
-import this
-from flask import Flask
-from flask import request
 import logging
 
-app = Flask(__name__)
-
 print('This is test job 1. Hihi')
-a = 1 + 2
-app.logger.warning('Tuyen testing warning log'+str(a))
-
 message = 'Tuyen write a message: Hello from test_job_1.py'
-
 def get_message():
     return message
 
 
 logging.warning("This is a warning!")
+raise NotImplementedError(f"An example error")
