a38a600a13c9af42f0fbbc64aff47e454f0ea5ab
tuyentran <tuyen.tran@trustingsocial.com>
2022-09-23 11:14:45 +0700

    remove importlib

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-
+#%%
 from flask import Flask
 from flask import request
 import importlib
@@ -37,3 +37,5 @@ def printMsg():
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
     
+
+# %%
