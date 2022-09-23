c63a8ce15ba8a8d7a6eb4c42584cb2b16a8cf6ac
tuyentran <tuyen.tran@trustingsocial.com>
2022-09-23 10:08:01 +0700

    add creds function

---
+++ jobs/test_ping.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import numpy as np
+
+def simple_ping(message):
+    return "Job simple_ping run. Message is: " + message
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask
 from flask import request
+from jobs import 
 
 app = Flask(__name__)
 
@@ -11,12 +12,18 @@ def hello_world():
     name = os.environ.get("NAME", "World")
     username = request.args.get('username')
     password = request.args.get('password')
-    return "Hello {}! Your username is {}".format(name, username)
+    hungcreds = os.environ.get("hunggmail")
+    return "Hello {}! Your username is {}. Hung Creds is {}".format(name, username,hungcreds)
     
 @app.route('/runjob', methods=['GET', 'POST'])
 def runjob():
     jobname = request.args.get('jobname')
     return "Running job" + jobname
     
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    
